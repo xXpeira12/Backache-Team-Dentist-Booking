f7ba358f25b9f2a8dc33be80e2afa173c1fdd70c
Sirasit Tanajirawat <nongyokkok@gmail.com>
2024-02-26 19:51:30 +0700

    Implement controllers/auth.js : sendTokenResponse and register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,46 @@
 const User = require('../models/User');
 
-//@desc     ...
-//@route    ... /api/v1/...
-//@access   ...
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+    // Create token
+    const token = user.getSignedJwtToken();
+
+    const options = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE*24*60*60*1000),
+        httpOnly: true
+    };
+
+    if(process.env.NODE_ENV === 'production'){
+        options.secure = true;
+    }
+
+    res.status(statusCode).cookie('token', token, options).json({
+        success: true,
+        token
+    });
+};
+
+// @desc    Register user
+// @route   POST /api/v1/auth/register
+// @access  Public
+exports.register = async (req,res,next) => {
+    try {
+        const {name, email, password, role} = req.body;
+
+        // Create user
+        const user = await User.create({
+            name,
+            email,
+            password,
+            role
+        });
+
+        sendTokenResponse(user, 200, res);
+
+    } catch(err) {
+        res.status(400).json({
+            success: false
+        });
+        console.log(err.stack);
+    }
+};
