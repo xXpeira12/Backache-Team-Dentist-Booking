b986c0cabe2415f3d7235cd9a8fa0427a9346363
Sirasit Tanajirawat <nongyokkok@gmail.com>
2024-02-27 16:43:38 +0700

    Implement controllers/auth.js Logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -93,3 +93,18 @@ exports.getMe = async (req,res,next) => {
         data: user
     });
 };
+
+// @desc    Log user out /  clear cookie
+// @route   POST /api/v1/auth/logout
+// @access  Private
+exports.logout = async (req,res,next) => {
+    res.cookie('token', 'none', {
+        expires: new Date(Date.now() + 10*1000),
+        httpOnly: true
+    });
+
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
+};
