6fc7b2f56937515b43a3517bca6b16de66ff90f9
Jaitnipat <jaitnipat@Jaitnipats-MacBook-Air.local>
2024-02-27 22:34:30 +0700

    Add notion that have to do in dentist

--- controllers/dentists.js
+++ controllers/dentists.js
@@ -4,12 +4,12 @@ const Dentist = require("../models/Dentist");
 //@route    ... /api/v1/...
 //@access   ...
 
-//Include other resource routers                        // Game
-// const bookingRouter = require("./bookings");         // Game
+//Include other resource routers             // Game
+const bookingRouter = require("./bookings"); // Game
 
-//Re-route into other resource routers                  // Game
-// router.use("/:dentistId/bookings", bookingRouter);   // Game
+//Re-route into other resource routers             // Game
+router.use("/:dentistId/bookings", bookingRouter); // Game
 
-//@desc     Do chapter 9 page 22                        // Game
+//TODO!     Do chapter 9 page 22      // Game
 
-//@desc     Do chapter 9 page 26                        // Game
+//TODO!     Do chapter 9 page 26      // Game
