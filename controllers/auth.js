9d3a8404258339ecc05a65b1e52f1bb64ea2c7ed
Sirasit Tanajirawat <nongyokkok@gmail.com>
2024-02-27 14:46:54 +0700

    Implement controllers/auth.js Get current Logged In user

--- controllers/auth.js
+++ controllers/auth.js
@@ -82,3 +82,14 @@ exports.login = async (req,res,next) => {
         sendTokenResponse(user, 200, res);
     }
 };
+
+// @desc    Get current Logged in user
+// @route   POST /api/v1/auth/me
+// @access  Private
+exports.getMe = async (req,res,next) => {
+    const user = await User.findById(req.user.id);
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+};
