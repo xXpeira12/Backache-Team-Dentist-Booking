8ce085b1ebed4491c6452d1dcde443d992ea443c
Sirasit Tanajirawat <nongyokkok@gmail.com>
2024-02-27 12:57:02 +0700

    Implement controllers/auth.js Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,3 +45,40 @@ exports.register = async (req,res,next) => {
         console.log(err.stack);
     }
 };
+
+// @desc    Login user
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = async (req,res,next) => {
+    const {email, password} = req.body;
+    
+    // Validate email & password
+    if(!email || !password){
+        return res.status(400).json({
+            success: false,
+            msg: 'Please provide an email and password'
+        });
+    }
+
+    // Check for user
+    const user = await User.findOne({email}).select('+password');
+
+    if(!user){
+        return res.status(400).json({
+            success: false,
+            msg: 'Invalid credentials'
+        });
+    }
+
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+
+    if(!isMatch){
+        return res.status(400).json({
+            success: false,
+            msg: 'Invalid credentials'
+        });
+    } else{
+        sendTokenResponse(user, 200, res);
+    }
+};
