62470843e7ff64d483c49646e2ca71348ddb5bc0
Sirasit Tanajirawat <nongyokkok@gmail.com>
2024-02-27 16:44:29 +0700

    Router of Logout in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -7,5 +7,6 @@ const {protect} = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.get('/logout', logout);
 
 module.exports = router;
