89ee6b3380bb24d6a6a7414d371c995ca18f21cb
Sirasit Tanajirawat <nongyokkok@gmail.com>
2024-03-24 17:02:38 +0700

    Addition Respone Result of sendTokenResponse

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,6 +16,9 @@ const sendTokenResponse = (user, statusCode, res) => {
 
     res.status(statusCode).cookie('token', token, options).json({
         success: true,
+        _id: user._id,
+        name: user.name,
+        email: user.email,
         token
     });
 };
